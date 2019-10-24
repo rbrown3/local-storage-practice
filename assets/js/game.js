894d302cc59a321478f9837a8efecb7b82712ac2
Bad Bitch <rosannabrown@outlook.com>
2019-10-24 18:02:11 -0400

    first commit

--- assets/js/game.js
+++
@@ -1,6 +0,0 @@
-const question = document.getElementById('question');
-const choices = Array.from(document.getElementsByClassName("choice-text"))
-console.log(choices)
-
-let currentQuestion = {}
-let acceptingAnswers = true;
